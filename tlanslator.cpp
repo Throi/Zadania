efb50344b698b4aee3dca67f9f1c8dfa6f729254
Throi <88439060+Throi@users.noreply.github.com>
2021-08-26 09:48:11 +0200

    Update tlanslator.cpp

--- tlanslator.cpp
+++ tlanslator.cpp
@@ -1,5 +1,101 @@
 #include <iostream>
 using namespace std;
+int power(int base, int pow)
+{
+    int result, i;
+    result = base;
+    for(i=1;i<pow;i++)
+    {
+        base = base * result;
+    }
+    if(pow=1)
+    {
+        result = base;
+    }
+    if(pow=0)
+    {
+        result = 1;
+    }
+    return result;
+}
+void translatedec2bin(int dec)
+{
+    int tablica[100];
+    int i,o,y,x;
+    x=dec;
+    while (x>=1)
+    {   
+        y = x%2; 
+        tablica[o] = y;
+        o++;
+        x = x/2;    
+    }
+    cout << endl << "dwojkowy: ";
+    for(i=o-1;i>=0;i--)
+    {
+        cout << tablica[i];
+    }
+
+}
+void translatedec2Oct(int dec)
+{
+    int x,y,i,o,tablica[100];
+    x=dec;
+    while(x>=1)
+    {   
+        y = x%8; 
+        tablica[o] = y;
+        o++;
+        x = x/8;    
+    }
+    cout << endl << "osemkowy: ";
+    for(i=o-1;i>=0;i--)
+    {
+        cout << tablica[i];
+    }
+    
+}
+
+void translatedec2Hex(int dec)
+{
+    int o,i,y,x, tablica[100];
+    x=dec;
+  while(x>=1)
+    {   
+        y = x%16; 
+        tablica[o] = y;
+        o++;
+        x = x/16;    
+    }
+    cout << endl << "szestnastkowy: ";
+        for(i=o-1;i>=0;i--)
+        {
+            switch (tablica[i])
+            {
+            case 10:
+                cout << "A";
+                break;
+            case 11:
+                cout << "B";
+                break;
+            case 12:
+                cout << "C";
+                break;
+            case 13:
+                cout << "D";
+                break;
+            case 14:
+                cout << "E";
+                break;
+            case 15:
+                cout << "F";
+                break;
+            default:
+                cout << tablica[i];
+                break;
+            }
+        }  
+}
 
 void translate(int x)
 {
@@ -7,7 +103,7 @@ void translate(int x)
     cout << x;
     int y,i,tablica[100] ;
     int o=0,S=x; 
-    int tab2[100] = { 1, 0, 1, 0};
+    int tab2[100] = { 1, 1, 0, 1, 0};
     while (x>=1)
     {   
         y = x%2; 
@@ -22,7 +118,6 @@ void translate(int x)
     }
     
     //ósemkowy
-    
     o=0,x=S;    
     while(x>=1)
     {   
@@ -39,7 +134,7 @@ void translate(int x)
 
     //szestnastkowy
 
-        o=0,x=S;    
+       o=0,x=S;    
     while(x>=1)
     {   
         y = x%16; 
@@ -78,15 +173,15 @@ void translate(int x)
     
     //dwojkowy na dziesietny
     cout << endl ; //"dwoj na dzies";
-    int L=3;
+    int L=5;
     o=0;
     x=0;
-    for(i=L;i>=0;i--)
+    for(i=L-1;i>=0;i--)
     {
-        /*switch (tab2[i])
+        switch (tab2[i])
         {
         case  1 :
-            x=+2^o;
+            x = x + power(2,o);
             o++;
             break;
         case 0 :
@@ -95,54 +190,39 @@ void translate(int x)
         default:
         cout << "blad";
             break;
-        }*/
-        if (tab2[i]==1)
-        {
-            x = x + pow(2,o);
-            o++;
-            cout << i << " jeden " << x << " " << o << endl;
         }
-        if (tab2[i]==0)
-        {
-            o++;
-            cout << i << " zero " << x << " " << o << endl;
-        }
-        if (tab2[i]>1)
-        {
-            cout << "blad";
-            cout << i << " " << x << endl;
-        }    
-    }
-    //cout << x;
-
-
     
+    }
+    cout << x;
 
 }
 
-void ShowResults()
+
+
+
+
+
+
+
+
+
+
+
+
+void ShowResults(int x)
 {
+    translatedec2bin(x);
+    translatedec2Oct(x);
+    translatedec2Oct(x);
     
 
 }
 
 int main()
 {
-    translate(2000);
+    //ShowResults(500);
+    //translatedec2Oct(500);
+    //translate(100);
 
     return 0;    
-}
-
-int pow(int base, int pow)
-{
-    int result, i;
-    for(i=0;i<pow;i++)
-    {
-        base = base * result;
-    }
-    if(base=0)
-    {
-        result = 1;
-    }
-    return result;
 }
